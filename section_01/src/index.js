f6ae6de0b6c93607f106cf335f95ce146c89fd8d
Jorge Alegretti <jorgeluiz.b.alegretti@gmail.com>
2021-09-27 01:24:25 -0300

    feat: start server

--- section_01/src/index.js
+++ section_01/src/index.js
@@ -1,1 +1,15 @@
-console.log('Hello, World !');
+import { ApolloServer, gql } from 'apollo-server';
+import { ApolloServerPluginLandingPageGraphQLPlayground } from 'apollo-server-core';
+
+const server = new ApolloServer({
+  typeDefs: gql`
+    type Query {
+      hello: String
+    }
+  `,
+  plugins: [ApolloServerPluginLandingPageGraphQLPlayground()],
+});
+
+server.listen(4003).then(({ url }) => {
+  console.log(`🚀  Server listening on url ${url}graphql`);
+});
